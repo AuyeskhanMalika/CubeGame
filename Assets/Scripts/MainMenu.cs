1ac8bad1872902f65ad06bb3956319cabc6b8e06
AuyeskhanM <auyeskhanm@gmail.com>
2020-02-10 16:55:03 +0600

    Game

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private Button _startButton, _quitButton;
+
+    private void Start()
+    {
+        startButton.onClick.AddListener(StartGame);
+        exitButton.onClick.AddListener(ExitGame);
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    private void OnDestroy()
+    {
+        startButton.onClick.RemoveAllListeners();
+        exitButton.onClick.RemoveAllListeners();
+    }
+}
---
+++ Assets/Scripts/WinPanel.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WinPanel : MonoBehaviour
+{
+    [SerializeField]
+    private Transform root;
+
+    [SerializeField]
+    private Text yourScoreText, bestScoreText;
+
+    [SerializeField]
+    private Button nextLevelButton, exitButton;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        root.gameObject.SetActive(false);
+
+        nextLevelButton.onClick.AddListener(OnNextLevelClick);
+        exitButton.onClick.AddListener(OnExitClick);
+    }
+
+    public void SetPanelActive(bool state)
+    {
+        root.gameObject.SetActive(state);
+    }
+
+    public void SetScoreText(int score)
+    {
+        int bestScore = PlayerPrefs.GetInt("BestScore");
+        if(score > bestScore)
+        {
+            PlayerPrefs.SetInt("BestScore", score);
+            bestScore = score;
+        }
+
+        yourScoreText.text = "Score: " + score.ToString();
+        bestScoreText.text = "Best score: " + bestScore;
+    }
+
+    private void OnNextLevelClick()
+    {
+        SceneManager.LoadScene(2);
+    }
+    private void OnExitClick()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void OnDestroy()
+    {
+        nextLevelButton.onClick.RemoveAllListeners();
+        exitButton.onClick.RemoveAllListeners();
+    }
+}
